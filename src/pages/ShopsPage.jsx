9710b485508f9313b419425061a13ae56582cd20
Egle <132908296+EgleUrbone@users.noreply.github.com>
2023-09-07 15:35:44 +0300

    ShopsPage getAllShops + empty ShopCardList

--- src/pages/ShopsPage.jsx
+++ src/pages/ShopsPage.jsx
@@ -1,7 +1,39 @@
-
+import { collection, getDocs } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../firebase/firebase';
+import ShopCardList from '../components/shops/ShopCardList';
 
 export default function ShopsPage() {
+  const [shopsArr, setShopsArr] = useState([]);
+
+  console.log('shopsArr ===', shopsArr);
+
+  async function getAllShopsFB() {
+    try {
+      const querySnapshot = await getDocs(collection(db, 'shops'));
+      const dataBack = [];
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        // console.log(doc.id, ' => ', doc.data());
+        const singleShop = {
+          id: doc.id,
+          ...doc.data(),
+        };
+        dataBack.push(singleShop);
+        setShopsArr(dataBack);
+      });
+    } catch (error) {
+      console.log('error ===', error);
+    }
+  }
+
+  useEffect(() => {
+    getAllShopsFB();
+  }, []);
+
   return (
-    <div>ShopsPage</div>
-  )
+    <div>
+      <ShopCardList list={shopsArr} />
+    </div>
+  );
 }
