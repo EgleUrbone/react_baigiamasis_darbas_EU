33b6e134805bd6dfa2e71ef2734071c4021a9905
Egle <132908296+EgleUrbone@users.noreply.github.com>
2023-09-10 21:48:28 +0300

    sort by year

--- src/pages/ShopsPage.jsx
+++ src/pages/ShopsPage.jsx
@@ -2,12 +2,15 @@ import { collection, getDocs } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import { db } from '../firebase/firebase';
 import ShopCardList from '../components/shops/ShopCardList';
+import { IoCalendarNumberOutline } from 'react-icons/io5';
+import { TbCircleLetterA } from 'react-icons/tb';
 
 export default function ShopsPage() {
   const [shopsArr, setShopsArr] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [sortedByYear, setSortedByYear] = useState([]);
 
-  console.log('shopsArr ===', shopsArr);
+  // console.log('shopsArr ===', shopsArr);
 
   async function getAllShopsFB() {
     try {
@@ -38,6 +41,14 @@ export default function ShopsPage() {
     }, 1000);
   }, []);
 
+  function sortArray() {
+    const sorted = [...shopsArr];
+    shopsArr.sort((a, b) => {
+      return a.year - b.year;
+    });
+    setSortedByYear(sorted);
+  }
+
   return (
     <div className='mt-[130px] lg:ml-auto lg:mr-auto'>
       <h1
@@ -46,6 +57,14 @@ export default function ShopsPage() {
       >
         Check out our Shops
       </h1>
+      <section>
+        <button onClick={sortArray}>
+          <IoCalendarNumberOutline />
+        </button>
+        <button>
+          <TbCircleLetterA />
+        </button>
+      </section>
       {isLoading ? <p className='text-center mt-2 mb-2'>Loading...</p> : null}
       {shopsArr.length === 0 && (
         <p className='text-center mt-2'>There are no shops to view..</p>
